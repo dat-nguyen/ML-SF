f6a6bd5ac3b222b1335d82d97e369267e4b9d0ab
Dat Nguyen <quocdat.nguyen@gmail.com>
2016-02-20 23:31:51 +0100

    Add classifyTest for all targets

--- weka/createMLScoring.py
+++ weka/createMLScoring.py
@@ -1,3 +1,6 @@
+#!/home/dat/libs/miniconda3/bin/python3.5
+# export PYTHONPATH=/mnt/zeus/dat/WORK/dev/ML-SF:$PYTHONPATH
+
 __author__ = 'dat'
 
 '''
@@ -14,9 +17,9 @@ testPath        = "/home/dat/WORK/arff/"
 #resultPath        = "/home/dat/WORK/arff/"
 resultPath      = "/home/dat/WORK/RESULTS/2016-02-23/"
 
-PREFIX_JAVA     = "java -Xmx32000M "
+PREFIX_JAVA     = "java -Xmx30000M "
 THREADNUM       = 8
-ITERATION       = 66
+ITERATION       = 100
 CLASSIFI        = "-t train.arff -T test.arff -c 1 -no-cv -o -classifications "
 OUTPUT_CSV      = "\"weka.classifiers.evaluation.output.prediction.CSV\""
 FILTER_PCA      = "\"weka.filters.unsupervised.attribute.PrincipalComponents -R 1.0 -A 5 -M -1\""
@@ -50,7 +53,7 @@ suffixTrainModel     = " -c 1 -no-cv "
                 #cmdDumpModel1 + dumpModel + cmdDumpModel2 + postCmdRoF + postCmdRoF_Methods[i] + " > dummy_stats.txt \n"
 
 prefixTestModel     = " -l "
-suffixTestModel     = " -T test.arff -c 1 -v -o -classifications " + OUTPUT_CSV + " "
+suffixTestModel     = " -T {0} -c 1 -v -o -classifications " + OUTPUT_CSV + " "
 
 #CMD_TEST = PREFIX_JAVA + metaClassifier + prefixTestModel + dumpModel + suffixTestModel + " > " + resultName + "\n"
 #            line = cmdTestModel1 + dumpModel + cmdTestModel2 + " > " + resultName + "\n"
@@ -61,7 +64,7 @@ suffixTestModel     = " -T test.arff -c 1 -v -o -classifications " + OUTPUT_CSV 
 
 descList        = ["elementsv2-SIFt"]#, "elementsv2-SIFt-xscore"]
 CASF_SETS       = ["CASFv2007", "CASFv2013-refined"]#, "CASF14"]
-trainingList    = ["sampling_clusters10", "sampling_100"]
+TRAIN_SETS      = ["sampling_clusters10", "sampling_100"]
 
 #################################################################
 def convertCSV2ARFF(CSVfile, ARFFpath):
@@ -125,8 +128,8 @@ def createTrainingModel(batchFile, CASFset, trainingPrefix, classifier = "Rotati
 
 
     SHFILE.close()
-
-def classifyTestModel(batchFile, testSet, CASFset, trainingPrefix, classifier = "RotationForest"):
+#################################################################
+def classifyTestModel(batchFile, testSet, CASFset, trainingPrefix, classifier = "RotationForest", tmpTestFile = "test.arff"):
     SHFILE  = open(batchFile, 'a')
     SHFILE.write("export CLASSPATH=/home/dat/WORK/dev/weka-3-7-12/weka.jar\n")
 
@@ -137,24 +140,36 @@ def classifyTestModel(batchFile, testSet, CASFset, trainingPrefix, classifier = 
             print("Test file not found ",testName)
             quit()
 
-        line = cmdRemove.format(testName, "test.arff")
+        line = cmdRemove.format(testName, tmpTestFile)
         print(line)
         SHFILE.write(line)
 
-        for base in baseClassifier.keys():
-            dumpModel = os.path.join(testPath, "test_model", "{0}_RMSD_{1}_{2}_{3}-{4}.model"
-                                     .format(CASFset, trainingPrefix, shortName[classifier], base, desc))
+        if classifier != "RandomForest":
+            for base in baseClassifier.keys():
+                dumpModel = os.path.join(testPath, "model", "{0}_RMSD_{1}_{2}_{3}-{4}.model"
+                                         .format(CASFset, trainingPrefix, shortName[classifier], base, desc))
+                if not os.path.exists(dumpModel):
+                    print("Training model not found ",dumpModel)
+                    quit()
+                resultName = os.path.join(resultPath, "{0}_{1}_{2}_{3}_{4}-{5}.csv".
+                                          format(CASFset, trainingPrefix, shortName[classifier], base, testSet, desc))
+                CMD_TEST = PREFIX_JAVA + metaClassifier[classifier] + prefixTestModel + dumpModel + suffixTestModel.format(tmpTestFile) + " > " + resultName + "\n"
+                print(CMD_TEST)
+                SHFILE.write(CMD_TEST)
+        else:
+            dumpModel = os.path.join(testPath, "model", "{0}_RMSD_{1}_{2}-{3}.model"
+                                     .format(CASFset, trainingPrefix, shortName[classifier], desc))
             if not os.path.exists(dumpModel):
                 print("Training model not found ",dumpModel)
                 quit()
-            resultName = os.path.join(resultPath, "{0}_{1}_{2}_{3}_{4}-{5}.csv".
-                                      format(CASFset, trainingPrefix, shortName[classifier], base, testSet, desc))
-            CMD_TEST = PREFIX_JAVA + metaClassifier[classifier] + prefixTestModel + dumpModel + suffixTestModel + " > " + resultName + "\n"
+            resultName = os.path.join(resultPath, "{0}_{1}_{2}_{3}-{4}.csv".
+                                      format(CASFset, trainingPrefix, shortName[classifier], testSet, desc))
+            CMD_TEST = PREFIX_JAVA + metaClassifier[classifier] + prefixTestModel + dumpModel + suffixTestModel.format(tmpTestFile) + " > " + resultName + "\n"
             print(CMD_TEST)
             SHFILE.write(CMD_TEST)
 
     SHFILE.close()
-
+#################################################################
 def CSAR():
     batchFile = "/home/dat/WORK/dev/weka-3-7-12/performScoring_DIG"
     DBSet = ["SP", "XP", "asp", "plp", "chemscore", "goldscore"]
@@ -170,14 +185,14 @@ def CSAR():
     #JelenaAllSets = ["140722_set1_maestro_", "140722_set2_maestro_", "140722_set3_maestro_"]
     #for eachset in JelenaAllSets:
      #   create<ClassifyScript(batchFile, eachset)
-
+#################################################################
 def DUDE():
     batchFile = "/home/dat/WORK/RMSD_DUDE/performScoring_DUDE"
     DBSet = ["RENI", "FGFR1", "ADA"]
     for eachset in DBSet:
         classifyTestModel(batchFile+"_test.sh", trainingSet="_refined_RMSD_", DBsetPrefix="DUD-E_", DBsetPostfix=eachset)
         #classifyTestModel(batchFile+"_test.sh", trainingSet="_reduced_RMSD_", DBsetPrefix="DUD-E_", DBsetPostfix=eachset)<
-
+#################################################################
 def RMSD():
     batchFile = "/home/dat/WORK/RMSD/performScoring_RMSD"
     #createTrainingModel(batchFile+"_training.sh", trainingSet="_refined_RMSD_")
@@ -186,25 +201,64 @@ def RMSD():
     for eachset in DBSet:
         #classifyTestModel(batchFile+"_test.sh", trainingSet="_reduced_RMSD_", DBsetPrefix="DIG10.2_", DBsetPostfix=eachset)
         classifyTestModel(batchFile+"_test.sh", trainingSet="_refined_RMSD_", DBsetPrefix="DIG10.2_", DBsetPostfix=eachset)
+#################################################################
+def createTrainAll():
+    #batchFile = "/home/dat/WORK/dev/weka-3-7-12/train_{0}.sh".format(CASF_SETS[0])
+    #createTrainingModel(batchFile, CASFset=CASF_SETS[0], trainingPrefix=TRAIN_SETS[0], tmpTrainFile="train.arff")
 
+    for CASFset in CASF_SETS:
+        for trainSet in TRAIN_SETS:
+            batchFile = "/home/dat/WORK/dev/weka-3-7-12/train_{0}.sh".format(CASFset)
+            createTrainingModel(batchFile, CASFset, trainingPrefix=trainSet, tmpTrainFile="train_{0}.arff".format(CASFset))
+            createTrainingModel(batchFile, CASFset, trainingPrefix=trainSet, classifier="RandomForest", tmpTrainFile="train_{0}.arff".format(CASFset))
+#################################################################
+def createTestAll():
+    #batchFile = "/home/dat/WORK/dev/weka-3-7-12/test{0}".format(CASFset)
+    #classifyTestModel(batchFile+"_KDMs.sh", testSet = "T36_JMJ_Xray_test_gold", CASFset=CASF_SETS[0], trainingPrefix=trainingList[0])
+
+    for CASFset in CASF_SETS:
+        for trainSet in TRAIN_SETS:
+            batchFile = "/home/dat/WORK/dev/weka-3-7-12/test{0}.sh".format(CASFset)
+            ############### Sirt2 from Berin ###############
+            testSet = "Aminothiazoles_71_PB_md1_rst_Dat"
+            classifyTestModel(batchFile, testSet, CASFset=CASFset, trainingPrefix=trainSet, tmpTestFile="test_{0}.arff".format(CASFset))
+            classifyTestModel(batchFile, testSet, CASFset=CASFset, trainingPrefix=trainSet, classifier="RandomForest", tmpTestFile="test_{0}.arff".format(CASFset))
+            ############### SmHDAC8 from Jelena ###############
+            testSet = "SmHDAC8_inhibitors"
+            classifyTestModel(batchFile, testSet, CASFset=CASFset, trainingPrefix=trainSet, tmpTestFile="test_{0}.arff".format(CASFset))
+            classifyTestModel(batchFile, testSet, CASFset=CASFset, trainingPrefix=trainSet, classifier="RandomForest", tmpTestFile="test_{0}.arff".format(CASFset))
+            ############### KDMs from Prof. Sippl ###############
+            for evalSet in ["test_gold", "actives", "inactives"]:
+                testSet = "T36_JMJ_Xray_{0}".format(evalSet)
+                classifyTestModel(batchFile, testSet, CASFset=CASFset, trainingPrefix=trainSet, tmpTestFile="test_{0}.arff".format(CASFset))
+                classifyTestModel(batchFile, testSet, CASFset=CASFset, trainingPrefix=trainSet, classifier="RandomForest", tmpTestFile="test_{0}.arff".format(CASFset))
+            ############### DIG from CSAR2013 ###############
+            for evalSet in ["XP", "SP", "goldscore"]:
+                testSet = "DIG_{0}".format(evalSet)
+                classifyTestModel(batchFile, testSet, CASFset=CASFset, trainingPrefix=trainSet, tmpTestFile="test_{0}.arff".format(CASFset))
+                classifyTestModel(batchFile, testSet, CASFset=CASFset, trainingPrefix=trainSet, classifier="RandomForest", tmpTestFile="test_{0}.arff".format(CASFset))
+#################################################################
+def createTestFidele():
+
+    TARGET_LIST_FIDELE  = ["5P21", "4BBG", "3KKP", "1GS4", "2X9E", "3E37"]
+    TARGET_DB_FIDELE    = ["africa", "npact"]
+    POSES_GEN_LIST_FIDELE  = ["GS", "XP"]
+    for CASFset in CASF_SETS:
+        for trainSet in TRAIN_SETS:
+            batchFile = "/home/dat/WORK/dev/weka-3-7-12/testFidele{0}.sh".format(CASFset)
+            ############### anti-cancer targets from Fidele ###############
+            for target in TARGET_LIST_FIDELE:
+                for target_db in TARGET_DB_FIDELE:
+                    for pose in POSES_GEN_LIST_FIDELE:
+                        testSet = "{0}-{1}-{2}".format(target, target_db, pose)
+                        classifyTestModel(batchFile, testSet, CASFset=CASFset, trainingPrefix=trainSet, tmpTestFile="test_{0}.arff".format(CASFset))
+                        classifyTestModel(batchFile, testSet, CASFset=CASFset, trainingPrefix=trainSet, classifier="RandomForest", tmpTestFile="test_{0}.arff".format(CASFset))
 ############# MAIN PART ########################
 if __name__=='__main__':
     '''
     '''
     #convertCSV2ARFF("/home/dat/WORK/DB/DESCRIPTORS/RMSD/processed/CASFv2007_RMSD_sampling_clusters10-elementsv2-SIFt.csv", "/home/dat/WORK/arff/")
-    #convertCSVDir2ARFF("/home/dat/WORK/DB/DESCRIPTORS/Processed/", "/home/dat/WORK/arff/")
-    #convertCSVDir2ARFF("/home/dat/WORK/DB/DESCRIPTORS/RMSD/Processed/", "/home/dat/WORK/arff/")
-
-    batchFile = "/home/dat/WORK/dev/weka-3-7-12/train_{0}.sh".format(CASF_SETS[0])
-    #batchFile = "/home/dat/WORK/dev/weka-3-7-12/train_{0}_{1}.sh".format(CASF_SETS[0], trainingList[0])
-    createTrainingModel(batchFile, CASFset=CASF_SETS[0], trainingPrefix=trainingList[0], tmpTrainFile="train.arff")
-
-    #batchFile = "/home/dat/WORK/dev/weka-3-7-12/train_{0}_{1}.sh".format(CASF_SETS[0], trainingList[1])
-    #createTrainingModel(batchFile, CASFset=CASF_SETS[0], trainingPrefix=trainingList[1], tmpTrainFile="train1.arff")
-    #createTrainingModel(batchFile, CASFset=CASF_SETS[0], trainingPrefix=trainingList[0], classifier="RandomForest", tmpTrainFile="train2.arff")
-    #createTrainingModel(batchFile, CASFset=CASF_SETS[0], trainingPrefix=trainingList[1], classifier="RandomForest", tmpTrainFile="train3.arff")
-
-
-    #batchFile = "/home/dat/WORK/dev/weka-3-7-12/testCASF07"
-    #classifyTestModel(batchFile+"_KDMs.sh", testSet = "T36_JMJ_Xray_test_gold", CASFset=CASF_SETS[0], trainingPrefix=trainingList[0])
-    #classifyTestModel(batchFile+"_KDMs.sh", testSet = "T36_JMJ_Xray_test_gold", CASFset=CASF_SETS[0], trainingPrefix=trainingList[1])
+    convertCSVDir2ARFF("/home/dat/WORK/DB/DESCRIPTORS/Processed/", "/home/dat/WORK/arff/")
+    #createTrainAll()
+    createTestFidele()
+    createTestAll()
